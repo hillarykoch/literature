7383fc4313a5a523a0e02f6572de50e742197f7b
nyck <niciagl@gmail.com>
2020-04-04 16:59:02 +0000

    fixed up deal, added member list of teams, added double-check for deal

--- literature/Literature.py
+++ literature/Literature.py
@@ -2,16 +2,46 @@ from .Cards import Deck, Hand
 from .People import Team
 
 class Literature:
+    NUM_PLAYERS = 6
+
     def __init__(self, team1, team2):
         self.deck = Deck()
         self.deck.shuffle()
-        self.deal(team1, team2)
 
-    def deal(self, team1, team2):
-        counter = 0
-        for tm in [team1, team2]:
+        self.teams = [team1, team2] 
+        self.deal()
+
+    def deal(self):
+        for tm in self.teams:
             for plr in tm.roster:
-#                plr.hand = [ self.deck.cards[i] for i in range(counter, counter + 9) ]
-#                for i in range(9):
-#                    plr.hand[i] = self.deck.cards[range(counter, counter + 9)[i]]
-#                    counter += 9
+                plr.hand = self.deck.cards[plr.position::Literature.NUM_PLAYERS]
+
+        # make sure this worked...
+        self.check_hands()
+
+################### HELPERS ####################
+    def check_hands(self):
+        print("Checking hands after deal")
+
+        all_cards = []
+        for tm in self.teams:
+            for plr in tm.roster:
+                all_cards += plr.hand
+
+        # a set can only have one of each item.  Would be shorter than the list if any dups
+        if len(all_cards) != len(set(all_cards)):
+            print("Duplicates!!")
+            raise ValueError
+
+
+################### "DEBUG" HELPERS ####################
+    def show_hands(self, team1, team2):
+        print()
+        print(f"{[c.get_card_name() for c in self.deck.cards]}")
+
+        for tm in [team1, team2]:
+            print()
+            print(f"{tm.team_name}")
+            for plr in tm.roster:
+                print(f"\t{plr.name}")
+                print(f"\t\t{[c.get_card_name() for c in plr.hand]}")
